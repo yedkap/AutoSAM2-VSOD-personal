26ba3bc05fae42c81dfa350894661a8d454e72ac
Atara Gold <yedkaplan@gmail.com>
2025-02-16 21:16:59 +0200

    updated dataset/tfs.py with transforms for the davsod dataset

--- dataset/tfs.py
+++ dataset/tfs.py
@@ -109,3 +109,26 @@ def get_polyp_transform():
         #                      [83.08, 55.86, 42.59])
     ])
     return transform_train, transform_test
+
+def get_davsod_transform():
+    transform_train = transforms.Compose([
+        # transforms.Resize((352, 352)),
+        transforms.ToPILImage(),
+        transforms.ColorJitter(brightness=0.4,
+                               contrast=0.4,
+                               saturation=0.4,
+                               hue=0.1),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomAffine(degrees=20, scale=(0.75, 1.25)),
+        transforms.ToTensor(),
+        # transforms.Normalize([105.61, 63.69, 45.67],
+        #                      [83.08, 55.86, 42.59])
+    ])
+    transform_test = transforms.Compose([
+        # transforms.Resize((352, 352)),
+        transforms.ToPILImage(),
+        transforms.ToTensor(),
+        # transforms.Normalize([105.61, 63.69, 45.67],
+        #                      [83.08, 55.86, 42.59])
+    ])
+    return transform_train, transform_test
