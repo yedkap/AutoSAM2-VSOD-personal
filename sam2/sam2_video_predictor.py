e0baaf9f7afeca4f8c4317bf69fea80c10de2df0
yedkap <49104309+yedkap@users.noreply.github.com>
2025-03-03 17:48:46 +0200

    Update sam2_video_predictor.py

--- sam2/sam2_video_predictor.py
+++ sam2/sam2_video_predictor.py
@@ -576,6 +576,7 @@ class SAM2VideoPredictor(SAM2Base):
         start_frame_idx=None,
         max_frame_num_to_track=None,
         reverse=False,
+        verbose=False,
     ):
         """Propagate the input points across frames to track in the entire video."""
         self.propagate_in_video_preflight(inference_state)
@@ -606,8 +607,9 @@ class SAM2VideoPredictor(SAM2Base):
                 start_frame_idx + max_frame_num_to_track, num_frames - 1
             )
             processing_order = range(start_frame_idx, end_frame_idx + 1)
-
-        for frame_idx in tqdm(processing_order, desc="propagate in video"):
+        
+        frames_iterator = tqdm(processing_order, desc="propagate in video") if verbose else processing_order
+        for frame_idx in frames_iterator:
             pred_masks_per_obj = [None] * batch_size
             for obj_idx in range(batch_size):
                 obj_output_dict = inference_state["output_dict_per_obj"][obj_idx]
