03c599636ef8facdba965cac52f53d05fddc7d38
Atara Gold <yedkaplan@gmail.com>
2025-03-05 21:01:08 +0200

    fixed bug in command line arguments

--- inference_video_rgbd.py
+++ inference_video_rgbd.py
@@ -283,13 +283,15 @@ if __name__ == '__main__':
     parser.add_argument('-depth_wise', '--depth_wise', default=False, help='unkown effect, model_single.py', required=False)
     parser.add_argument('-order', '--order', default=85, help='unkown effect, model_single.py', required=False)
     parser.add_argument('-Idim', '--Idim', default=512, help='image size', required=False)
-    parser.add_argument('--test_run', default=False, type=bool, help='if True, stops all train / eval loops after single iteration / input', required=False)
+    parser.add_argument('--test_run', default=0, type=int, help='if True, stops all train / eval loops after single iteration / input', required=False)
     parser.add_argument('--cutoff_eval', default=None, type=int, help='sets max length for eval datasets.', required=False)
     parser.add_argument('-folder', '--folder', help='image size', required=True)
     parser.add_argument('--dataset', default='easy', help='test dataset. easy, normal, hard, vidsod')
-    parser.add_argument('--use_depth', default=True, type=bool, help='If true, uses RGBD backbone for the prompt encoder')
+    parser.add_argument('--use_depth', default=1, type=int, help='If 1, uses RGBD backbone for the prompt encoder')
     args = vars(parser.parse_args())
 
+    args['use_depth'] = args['use_depth'] == 1
+    args['test_run'] = args['test_run'] == 1
     os.makedirs('results_test', exist_ok=True)
     folder_load = args['folder']
     args['results_root'] = open_folder('results_test')
