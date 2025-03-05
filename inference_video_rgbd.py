eaa079c98ff3cd01a17fa590f5cc1d081e7cfa3e
yedkap <49104309+yedkap@users.noreply.github.com>
2025-03-05 11:17:26 +0200

    Update inference_video_rgbd.py

--- inference_video_rgbd.py
+++ inference_video_rgbd.py
@@ -36,8 +36,8 @@ def Dice_loss(y_true, y_pred, smooth=1):
 
 def get_dice_ji(predict_scores, target_in, smooth=1e-8):
     target = target_in + 1
-    thresholds = np.linspace(0, 1, 21)
-    # thresholds = [2/3]
+    # thresholds = np.linspace(0, 1, 21)
+    thresholds = [2/3]
     f_betas = []
     for thresh in thresholds:
         predict = predict_scores.copy()
