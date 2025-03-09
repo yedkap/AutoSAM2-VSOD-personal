96881f8b0bdf8e1fdddc130bf8f90be8d4890eab
yedkap <49104309+yedkap@users.noreply.github.com>
2025-03-09 17:25:06 +0200

    temporary update inference.py

--- inference.py
+++ inference.py
@@ -246,8 +246,10 @@ def main(args=None, sam_args=None, test_run=False):
     else:
         model = ModelEmb(args=args, size_out=64, train_decoder_only=True).to(device)
 
-    state_dict = torch.load(args['path_best'])
-    model.load_state_dict(state_dict)
+    # state_dict = torch.load(args['path_best'])
+    # model.load_state_dict(state_dict)
+    model1 = torch.load(args['path_best'], weights_only=False)
+    model.load_state_dict(model1.state_dict())
     print('Trained weights loaded')
 
     model_cfg = sam_args['fp_config']
