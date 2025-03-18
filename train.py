20ffc698caaf86b08b38df89b7375897f870c3ee
Atara Gold <yedkaplan@gmail.com>
2025-03-18 16:00:58 +0200

    test commit

--- train.py
+++ train.py
@@ -373,7 +373,6 @@ def main(args=None, sam_args=None, test_run=False):
     else:
         device = torch.device("cpu")
 
-
     if args['use_optical_flow']:
         model = ModelEmbFusionLarge(
             args=args, size_out=64, train_decoder_only=args['decoder_only']
